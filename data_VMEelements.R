8201b2f977ea4fa579136b02dd2423290abf4a47
Neil Campbell <neil.campbell@ices.dk>
2024-01-10 14:07:24 +0100

    two-step approach is much quicker

--- data_VMEelements.R
+++ data_VMEelements.R
@@ -1,26 +1,44 @@
-elements <- st_read("physical_VME_elements.shp")
+library(raster)
+library(sf)
+library(vmstools)
+
+elements <- st_read("physical_VME_elements(fixed).shp")
 
 # Define the resolution
-res <- 0.005
+res <- 0.05
 
 # Create an empty raster with the desired resolution
 r <- raster(floor(extent(elements)), res=res)
 
+# Convert the polygon to a raster
+poly_ras <- rasterize(elements, r, field = 1)
+
+# Convert the polygon to MULTILINESTRING first
+poly_multilines <- st_cast(elements, "MULTILINESTRING")
+
+# Then cast the MULTILINESTRING to LINESTRING
+poly_lines <- st_cast(poly_multilines, "LINESTRING")
+
+# Rasterize the lines
+lines_ras <- rasterize(poly_lines, r, field = 1)
+
+# Combine the two rasters
+combined_ras <- merge(poly_ras, lines_ras)
+
 # Rasterize the polygon
-r_polygons <- rasterToPolygons(r)
+r_polygons <- rasterToPolygons(combined_ras)
 
 # Convert to sf objects
 r_polygons_sf <- st_as_sf(r_polygons)
-elements_sf <- st_as_sf(elements)
 
-# Identify which raster polygons intersect with the elements
-intersects <- st_intersects(r_polygons_sf, elements_sf)
+# Make the spatial objects valid
+r_polygons_sf <- st_make_valid(r_polygons_sf)
 
-# Filter the raster polygons that intersect with the elements
-r_polygons_intersect <- r_polygons_sf[unlist(intersects), ]
+# set the CRS
+r_polygons_sf <- st_set_crs(r_polygons_sf, st_crs(elements))
 
 # Calculate the centroid of each intersecting raster polygon
-centroids <- st_centroid(r_polygons_intersect)
+centroids <- st_centroid(r_polygons_sf)
 
 # Get the coordinates of these centroids
 coords <- st_coordinates(centroids)
@@ -35,6 +53,6 @@ df$csquare <- CSquare(df$Longitude, df$Latitude, 0.05)
 df$wkt <- wkt_csquare(df$Latitude, df$Longitude)
 df <- st_as_sf(df, wkt = "wkt")
 df <- df %>% st_set_crs(4326)
+
 # Write the data frame to a shapefile
 st_write(df, "elementscsquare.shp", append = F)
-
