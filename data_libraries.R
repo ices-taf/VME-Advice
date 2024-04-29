0012428c5e84182ebadf2d495b10dd0f6e34a998
Neil Campbell <neil.campbell@ices.dk>
2024-04-29 15:03:25 +0200

    don't need so many libraries (other than for the HTML files?)

--- data_libraries.R
+++ data_libraries.R
@@ -1,5 +1,3 @@
 # Install and load required packages using pacman
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(sf, rgdal, rgeos, maptools, leaflet, htmlwidgets, smoothr, 
-raster, tmap, dplyr, tidyr, knitr, kableExtra, htmlTable, glue, icesTAF,
-data.table, nngeo, vmstools, icesVMS)
+pacman::p_load(sf, raster, dplyr, purrr, nngeo, icesTAF, icesVMS, vmstools)
