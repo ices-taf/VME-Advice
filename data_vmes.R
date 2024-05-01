f5f93f36d578a51d0e8d9c4ac6bd13e17e5e9a0c
Neil Campbell <neil.campbell@ices.dk>
2024-05-01 15:03:20 +0200

    added tidyr to the libraries, cleaned up some unused steps

--- data_vmes.R
+++ data_vmes.R
@@ -1,19 +1,6 @@
 # read the VME data file
 vme_observations <- read.csv("VME_observations_datacall_2022_eu.csv", header=T)
 
-# process the VME data
-vme <- getCSquare(vme_observations$MiddleLatitude, vme_observations$MiddleLongitude, 0.05)
-vme <- unique(vme)
-vme <- cbind(vme, CSquare2LonLat(vme, 0.05))
-names(vme) <- c("csquare", "Latitude", "Longitude")
-vme$wkt <- wkt_csquare(vme$Latitude, vme$Longitude)
-vme_csquare <- vme %>%
-  dplyr::select("csquare", "wkt") %>%
-  st_as_sf(wkt = "wkt") %>%
-  st_set_crs(4326)
-
-# Write the data frame to a shapefile
-st_write(vme_csquare, "vme_csquares.shp", append = FALSE)
 
 vme_records <- st_as_sf(vme_observations, coords = c("MiddleLongitude", "MiddleLatitude"), crs = 4326) %>%
   dplyr::select(VME_Indicator, geometry) %>%
