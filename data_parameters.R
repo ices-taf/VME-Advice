c578f4216967531245537276973d3c46b646c300
Neil Campbell <neil.campbell@ices.dk>
2024-12-02 15:50:10 +0100

    updated assessment years

--- data_parameters.R
+++ data_parameters.R
@@ -1,7 +1,7 @@
 # Set data call years to work with for VME and VMS data
-datacallyear     <- 2022   # VME data call year with latest data
-datacallyear_VMS <- 2022   # VMS data call year with latest data
+datacallyear     <- 2024   # VME data call year with latest data
+datacallyear_VMS <- 2023   # VMS data call year with latest data
 
 # Set a variable for the previous year
-assyearprev <- 2020
+assyearprev <- 2022
 
